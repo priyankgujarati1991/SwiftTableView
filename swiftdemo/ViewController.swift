c297eb224fddf7ce973e0aa4aaa3ba25cd07a669
Priyank <priyank@Priyanks-MacBook-Pro.local>
2017-05-24 23:49:46 +0530

    Changes tableview

--- swiftdemo/ViewController.swift
+++ swiftdemo/ViewController.swift
@@ -8,16 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tbl: UITableView!
+    
+    var arr = [String]()
+    
+    let cellidentifier = "Cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        let filemanager = FileManager.default
+        arr = ["Apple","Mango","Orange","Banana"]
+        tbl.delegate = self
+        tbl.dataSource = self
+        tbl.register(UITableViewCell.self, forCellReuseIdentifier: cellidentifier)
+    
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        print("Hello world")
+        var cell = tbl.dequeueReusableCell(withIdentifier:cellidentifier)
         
+        if cell == nil  {
+            cell = UITableViewCell(style: .default, reuseIdentifier:cellidentifier)
+        }
+        cell?.textLabel?.text = arr[indexPath.row]
+//        cell!.backgroundColor = UIColor .green
+        
+        return cell!
     }
 
     override func didReceiveMemoryWarning() {
